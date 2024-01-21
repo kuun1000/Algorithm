3fb47368f5e3e461e833f030878d3299fc6b5e62
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-01-21 16:08:12 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n-1, -1, -1):
+    print(" " * i, end="") 
+    print("*" * (2 * (n - i) - 1))
+
+for j in range(1, n):
+    print(" " * j, end="")
+    print("*" * (2 * (n - j) - 1))
