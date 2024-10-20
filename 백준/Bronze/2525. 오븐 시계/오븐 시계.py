ada11804ab60bea285a95479c9409db0a289a64c
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-21 08:44:07 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,12 +1,7 @@
-A, B = tuple(map(int, input().split()))
-C = int(input())
+a, b = tuple(map(int, input().split()))
+c = int(input())
 
-current_time = A * 60 + B
-expect_time = current_time + C
+a = (a + ((b + c) // 60)) % 24
+b = (b + (c % 60)) % 60
 
-H, M = expect_time // 60, expect_time % 60
-
-if H >= 24:
-    H -= 24
-    
-print(f"{H} {M}")
+print(a, b)
