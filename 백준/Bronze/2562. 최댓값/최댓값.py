51abe4fdc20a9aa1afb4c2376625a2d0107459cc
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-22 08:21:23 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,10 +1,5 @@
-# 9개의 자연수 입력 받기
 arr = [int(input()) for _ in range(9)]
 
-# 최댓값
 max_val = max(arr)
-# 최댓값의 인덱스 + 1
-max_idx = arr.index(max_val) + 1
-
 print(max_val)
-print(max_idx)
+print(arr.index(max_val) + 1)
