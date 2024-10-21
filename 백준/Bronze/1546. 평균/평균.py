0853356c141966fa84a5df1823f59943722f469a
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-22 08:45:48 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,11 +1,6 @@
 n = int(input())
-score = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-# 최대 점수
-max_val = max(score)
-
-# 점수 조작
-manipulated = [(elem / max_val * 100) for elem in score]
-
-# 평균 계산
-print(sum(manipulated) / n)
+max_val = max(arr)
+result = [(i / max_val * 100) for i in arr]
+print(sum(result) / n)
