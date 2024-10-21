01ecf29cb95c65e16bb7b7c7474fcc3ee28b2ae9
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-22 08:29:38 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,14 +1,7 @@
 n, m = tuple(map(int, input().split()))
+arr = [0] * (n + 1)
 
-# 각 바구니에 들어있는 공의 번호
-basket = [0] * n
-
-# m개의 명령에 대해
 for _ in range(m):
-    # 각 명령을 입력 받음
     i, j, k = tuple(map(int, input().split()))
-    
-    # i-1 ~ j까지의 원소의 값을 k로 변경
-    basket[i-1:j] = [k] * (j - i + 1)
-    
-print(*basket)
+    arr[i:j+1] = [k] * (j - i + 1)
+print(*arr[1:])
