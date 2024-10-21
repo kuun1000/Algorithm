fddc3f08d9e0f2b6bcf7cba372c44d9b47efdd35
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-22 08:43:10 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,13 +1,7 @@
 n, m = tuple(map(int, input().split()))
+arr = [i for i in range(n+1)]
 
-# 바구니의 번호
-basket = [i for i in range(1, n+1)]
-
-# 각 명령에 대해
 for _ in range(m):
     i, j = tuple(map(int, input().split()))
-
-    # 범위에 해당하는 부분을 뒤집음
-    basket[i-1:j] = basket[i-1:j][::-1] 
-
-print(*basket)
+    arr[i:j+1] = arr[i:j+1][::-1]
+print(*arr[1:])    
