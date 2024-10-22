7b0be389da61c4c0b8b6a14894d98e3e029ed460
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-23 01:19:32 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,12 +1,2 @@
-s = input()
-length = len(s)
-
-if length % 2 == 0: # 단어의 길이가 짝수인 경우
-    # 앞 문자열과 뒤집은 뒷 문자열 비교
-    result = (s[:length // 2] == s[length // 2: ][::-1])
-
-else: # 단어의 길이가 홀수인 경우
-    # 가운데 문자를 제외하고, 앞 문자열과 뒤집은 뒷 문자열 비교
-    result = (s[:length // 2] == s[length // 2 + 1:][::-1])
-
-print(int(result))
+word = list(input())
+print(1 if word == word[::-1] else 0)
