fb317f388c5bc0fa178469a15e7d45be8ea11941
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-23 07:50:22 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,7 +1,6 @@
-# 5 x n 격자 입력 받기
-grid = [input() for _ in range(5)]
+arr = [input() for _ in range(5)]
 
-for j in range(15):     # 각 행에 대해
-    for i in range(5):      # 각 열에 대해 
-        if j < len(grid[i]):    # 행 인덱스 < 행 원소 개수
-            print(grid[i][j], end="")   # 해당 원소 출력
+for j in range(15):
+    for i in range(5):
+        if j < len(arr[i]):
+            print(arr[i][j], end="")
