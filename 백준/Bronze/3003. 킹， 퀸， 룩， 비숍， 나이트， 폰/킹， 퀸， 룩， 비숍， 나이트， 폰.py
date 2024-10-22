464d2793f386d655dd963ea7eabda9b8cc1b42b5
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-23 01:08:56 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,7 +1,4 @@
-# 올바른 피스
-piece = [1, 1, 2, 2, 2, 8]
-# 발견한 흰색 피스 
-white_piece = list(map(int, input().split()))
+correct = [1, 1, 2, 2, 2, 8]
+current = list(map(int, input().split()))
 
-# 올바른 피스 - 발견한 흰색 피스
-print(*[p - wp for p, wp in zip(piece, white_piece)])
+print(*[x - y for x, y in zip(correct, current)])
