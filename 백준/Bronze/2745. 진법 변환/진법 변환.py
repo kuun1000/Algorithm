81b6d218454efe5be237d046e2cb6e8e3e9a6614
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-23 08:20:50 +0900

    [Bronze II] Title: 진법 변환, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,23 +1,10 @@
-# 변수 선언 및 입력
 n, b = input().split()
-length = len(n) # 자릿수
 b = int(b)
 
-# b진법 수 중 알파벳 문자를 숫자로 변환
-digits = [0] * length   # b진법 수 
-for i in range(length):
-    if 'A' <= n[i] <= 'Z':
-        digits[i] = ord(n[i]) - 55
+num = 0
+for idx, digit in enumerate(n):
+    if digit.isalpha():
+        num += (ord(digit) - 55) * (b**(len(n) - idx - 1))
     else:
-        digits[i] = int(n[i])
-
-number = 0  # 10진수로 변환된 수 
-if b == 10: # 10진법으로 바꾸는 경우
-    number = n  # 그대로 출력
-
-else:   # 10진법 이외의 진법으로 바꾸는 경우
-    for i in range(length): # 각 자리수에 대해 
-        # 해당 자릿수의 값 X (진법의 제곱근)
-        number += (digits[i] * (b ** (length - i - 1))) 
-
-print(number)
+        num += int(digit) * (b**(len(n) - idx - 1))
+print(num)
