731960e64b5b554290bbdaf91b562a68979d9487
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-23 08:03:21 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,17 +1,9 @@
-# 100 x 100 격자
-grid = [[0 for _ in range(100)]
-        for _ in range(100)]
+grid = [[0] * 100 for _ in range(100)]
 
-n = int(input())    # 색종이의 수
-positions = [list(map(int, input().split())) for _ in range(n)]   # 색종이 붙인 위치
-
-# 각 색종이에 대해
-for position in positions:
-    # 좌측 하단 -> 좌측 상단으로 생각
-    x, y = position
-    
-    for i in range(x, x + 10):
-        for j in range(y, y + 10):
+for _ in range(int(input())):
+    w, h = tuple(map(int, input().split()))
+    for i in range(w, w+10):
+        for j in range(h, h+10):
             grid[i][j] = 1
-
-print(sum(sum(grid, [])))
+            
+print(sum(sum(grid, start=[])))
