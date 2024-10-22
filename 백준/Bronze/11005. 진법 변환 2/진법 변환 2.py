2f2ef66c1f475162d360e6c5da256679e1d4f46e
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-10-23 08:34:58 +0900

    [Bronze I] Title: 진법 변환 2, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,20 +1,12 @@
-import sys
+n, b = map(int, input().split())
 
-# 변수 입력 및 선언
-n, b = tuple(map(int, sys.stdin.readline().split()))
-digits = []
-
-# 10진법 -> N진법 변환
-while True:
-    if n < b:
-        digits.append(n)
-        break
-    digits.append(n % b)
-    n //= b
-
-# 10진법 넘어가는 진법인 경우, 알파벳 대문자 사용
-for digit in digits[::-1]:
-    if 10 <= digit <= 35:
-        print(chr(digit + 55), end="")
+ans = ''
+while n > 0:
+    remain = n % b  # n을 b로 나눈 나머지를 계산
+    if remain >= 10:
+        ans = chr(ord('A') + (remain - 10)) + ans  # 10 이상일 때 A~Z로 변환
     else:
-        print(digit, end="")
+        ans = str(remain) + ans  # 10 미만일 때 숫자를 그대로 추가
+    n //= b  # n을 b로 나눔
+
+print(ans)
