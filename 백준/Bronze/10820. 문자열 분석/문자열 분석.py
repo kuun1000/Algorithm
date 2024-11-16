762707ddb5fd3edd7285eb470711838172862c98
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-11-16 14:51:36 +0900

    [Bronze II] Title: 문자열 분석, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,16 +1,14 @@
-try:
-    while True:
-        s = list(input())
-        result = [0] * 4
-        for elem in s:
-            if ord("A") <= ord(elem) <= ord("Z"):
-                result[1] += 1
-            elif ord("a") <= ord(elem) <= ord("z"):
-                result[0] += 1
-            elif elem.isdigit():
-                result[2] += 1
-            elif elem == " ":
-                result[3] += 1
-        print(*result)
-except EOFError:
-    pass
+import sys
+
+# 입력 데이터 읽기
+lines = sys.stdin.read().splitlines()
+
+# 각 줄에 대해 처리
+for line in lines:
+    lower_count = sum(1 for char in line if char.islower())  # 소문자 개수
+    upper_count = sum(1 for char in line if char.isupper())  # 대문자 개수
+    digit_count = sum(1 for char in line if char.isdigit())  # 숫자 개수
+    space_count = sum(1 for char in line if char.isspace())  # 공백 개수
+    
+    # 결과 출력
+    print(lower_count, upper_count, digit_count, space_count)
