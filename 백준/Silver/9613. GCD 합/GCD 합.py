0948434e20c016e62c5a38ace6cbe9b898241f81
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-12-02 15:29:48 +0900

    [Silver IV] Title: GCD 합, Time: 164 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,16 +1,10 @@
+from math import gcd
 from itertools import combinations
-
-def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a %b)
     
 for _ in range(int(input())):
     n, *arr = map(int, input().split())
-    combi = list(combinations(arr, 2))
 
     result = 0
-    for a, b in combi:
+    for a, b in combinations(arr, 2):
         result += gcd(a, b)
     print(result)
