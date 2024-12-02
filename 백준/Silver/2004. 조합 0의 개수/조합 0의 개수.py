9e4b9a44ac95ed8d8d46b25e384665a6b4be4bd9
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-12-02 14:00:01 +0900

    [Silver II] Title: 조합 0의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -1,18 +1,18 @@
 def count_factors(n, p):
-    count = 0
+    cnt = 0
     while n >= p:
-        count += n // p
+        cnt += n // p
         n //= p
-    return count
+    return cnt
 
-def trailing_zeros_in_combination(n, m):
+def trailing_zeros(n, m):
     if m == 0 or n == m:
         return 0
-
-    count_2 = count_factors(n, 2) - count_factors(m, 2) - count_factors(n - m, 2)
-    count_5 = count_factors(n, 5) - count_factors(m, 5) - count_factors(n - m, 5)
     
-    return min(count_2, count_5)
+    cnt_2 = count_factors(n, 2) - count_factors(m, 2) - count_factors(n - m, 2)
+    cnt_5 = count_factors(n, 5) - count_factors(m, 5) - count_factors(n - m, 5)
+
+    return min(cnt_2, cnt_5)
 
 n, m = map(int, input().split())
-print(trailing_zeros_in_combination(n, m))
+print(trailing_zeros(n, m))
