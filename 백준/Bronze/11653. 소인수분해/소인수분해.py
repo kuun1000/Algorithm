18c33daebfdea2e041ff7e8069c99c7107273b09
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-12-09 09:43:13 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,9 +1,29 @@
-n = int(input())
+import math
 
-d = 2
-while n != 1:
-    if n % d != 0:
-        d += 1
-    else:
-        n //= d
-        print(d)
+def isPrime(limit):
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(math.sqrt(limit)) + 1):
+        if is_prime[i]:
+            for j in range(i * i, limit + 1, i):
+                is_prime[j] = False
+
+    return is_prime
+
+def prime_factorization(n):
+    limit = int(math.sqrt(n)) + 1
+    prime_list = isPrime(limit)
+
+    for i in range(2, len(prime_list)):
+        if prime_list[i]:
+            while n % i == 0:
+                print(i)
+                n //= i
+
+    if n > 1:
+        print(n)
+
+n = int(input())
+if n > 1:
+    prime_factorization(n)
