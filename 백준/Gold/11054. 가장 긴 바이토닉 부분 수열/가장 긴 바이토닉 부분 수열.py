ea1bf774f67e79e64622592b32ba4ab2b8af9d7d
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-01-08 08:00:25 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 224 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+
+lis = [1] * n
+for i in range(n):
+    for j in range(i):
+        if a[j] < a[i]:
+            lis[i] = max(lis[i], lis[j] + 1)
+
+lds = [1] * n
+for i in range(n - 1, -1, -1):
+    for j in range(n - 1, i, -1):
+        if a[j] < a[i]:
+            lds[i] = max(lds[i], lds[j] + 1)
+
+max_len = 0
+for i in range(n):
+    max_len = max(max_len, lis[i] + lds[i] - 1)
+
+print(max_len)    
