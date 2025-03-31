403747ef848cf99eebf5282b495fc84e099583a8
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-04-01 08:06:24 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def traverse(node, order):
+    if node == '.':
+        return ""
+    
+    result = ""
+    for char in order:
+        if char == "N":
+            result += node
+        elif char == "L":
+            result += traverse(tree[node][0], order)
+        elif char == "R":
+            result += traverse(tree[node][1], order)
+    return result
+
+
+
+n = int(input())
+tree = {}
+for _ in range (n):
+    node, left, right = input().split()
+    tree[node] = (left, right)
+
+print(traverse('A', 'NLR')) # 전위 순회
+print(traverse('A', 'LNR')) # 중위 순회
+print(traverse('A', 'LRN')) # 후위 순회
