4f549f1d0dc82e8d8bc97c89ac40d1a9a375e191
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-04-29 08:03:12 +0900

    [Silver I] Title: 에너지 모으기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -2,18 +2,20 @@ import sys
 input = sys.stdin.readline
 
 def dfs(sequence, current_sum):
-    global max_sum
     if len(sequence) == 2:
-        max_sum = max(max_sum, current_sum)
+        return current_sum
     
+    result = 0
     for i in range(1, len(sequence) - 1):
-        dfs(sequence[:i]+sequence[i+1:], current_sum + sequence[i-1] * sequence[i+1]) 
+        energy = sequence[i-1] * sequence[i+1]
+        removed = sequence.pop(i)
+        result = max(result, dfs(sequence, current_sum + energy))
+        sequence.insert(i, removed)
+    return result
 
 
 n = int(input())
 weights = list(map(int, input().split()))
 
-max_sum = 0 
-dfs(weights, 0)
-
+max_sum = dfs(weights, 0)
 print(max_sum)
