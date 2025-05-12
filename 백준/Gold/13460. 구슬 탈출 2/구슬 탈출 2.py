d6c89ef0f9ca11339e027bd7372d7a622d5ec3ca
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-05-12 14:38:33 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 120 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -2,58 +2,54 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def is_valid(x, y): # 유효 위치인지 확인하기
-    return 0 <= x < n and 0 <= y < m
 
-def roll(x, y, dir): # 구슬 굴리기
-    is_in_hole = False
+
+def roll(x, y, dx, dy):
     count = 0
-    while is_valid(x + dir[0], y + dir[1]):
-        nx, ny = x + dir[0], y + dir[1]
+    while True:
+        nx, ny = x + dx, y + dy
         if board[nx][ny] == '#':
             break
-        elif board[nx][ny] == 'O':
-            x, y = nx, ny
-            is_in_hole = True
-            break
+        if board[nx][ny] == 'O':
+            return nx, ny, count, True
         x, y = nx, ny
         count += 1
-    return x, y, count, is_in_hole
+    return x, y, count, False
 
 directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-def bfs(rx, ry, bx, by, count):
-    queue = deque([((rx, ry), (bx, by), count)])
+def bfs(rx, ry, bx, by):
+    queue = deque([((rx, ry), (bx, by), 0)])
+    visited[rx][ry][bx][by] = True
 
     while queue:
-        red, blue, count = queue.popleft()
+        (rx, ry), (bx, by), count = queue.popleft()
 
-        if blue == hole:
-            continue
-        if red == hole:
-            return count + 1
         if count >= 10:
             return -1
-        
-        for dir in directions:
-            nrx, nry, nrcount, r_in_hole = roll(*red, dir)
-            nbx, nby, nbcount, b_in_hole = roll(*blue, dir)
 
-            if b_in_hole:
+        for dx, dy in directions:
+            nrx, nry, r_dist, red_in = roll(rx, ry, dx, dy)
+            nbx, nby, b_dist, blue_in = roll(bx, by, dx, dy)
+
+            if blue_in:
                 continue
-            if r_in_hole:
+            if red_in:
                 return count + 1
 
             if nrx == nbx and nry == nby:
-                if nrcount > nbcount:
-                    nrx, nry = nrx - dir[0], nry - dir[1]
+                if r_dist > b_dist:
+                    nrx -= dx
+                    nry -= dy
                 else:
-                    nbx, nby = nbx - dir[0], nby - dir[1]
-            
+                    nbx -= dx
+                    nby -= dy
+
             if not visited[nrx][nry][nbx][nby]:
                 visited[nrx][nry][nbx][nby] = True
                 queue.append(((nrx, nry), (nbx, nby), count + 1))
     return -1
+
 
 
 # 입력 처리 및 상태 초기화
@@ -70,4 +66,4 @@ for i in range(n):
 visited = [[[[False for _ in range(m)] for _ in range(n)] for _ in range(m)] for _ in range(n)]
 visited[red[0]][red[1]][blue[0]][blue[1]] = True
 
-print(bfs(*red, *blue, 0))
+print(bfs(*red, *blue))
