e63dc5dca72519a042f9fb92f51acb3f8d4a710b
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-08-10 18:19:32 +0900

    [Bronze II] Title: Hashing, Time: 32 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+l = int(input())
+arr = list(map(lambda x:ord(x)-96, input().strip()))
+r, m = 31, 1234567891
+
+result = 0
+for idx, elem in enumerate(arr):
+    result += elem * (r**idx)
+
+print(result)
