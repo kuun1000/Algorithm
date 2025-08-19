2fae39f0ccb20019fd9719ec9758e279cd078b8c
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-08-19 18:46:22 +0900

    [Silver V] Title: 나이순 정렬, Time: 296 ms, Memory: 58308 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,7 +1,10 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-members = [[int(age), name, i] for i, (age, name) in enumerate(input().split() for _ in range(n))]
+arr = [[int(age), name, i] for i, (age, name) in enumerate(input().split() for _ in range(n))]
 
-members.sort(key=lambda members:(members[0], members[2]))
+arr.sort(key=lambda x: (x[0], x[2]))
 
-for member in members:
-    print(member[0], member[1])
+for elem in arr:
+    print(elem[0], elem[1])
